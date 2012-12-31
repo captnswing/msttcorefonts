7c8b7c598afda5523518ddf561f9cd9b24a274b7
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2012-12-31 17:14:05 +0100

    improved metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email "frank.hoffsummer@gmail.com"
 license          "Apache 2.0"
 description      "Installs Microsoft corefonts on linux boxes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.2"
 
 depends "yum"
+
+supports "scientific", ">= 6.0"
+supports "ubuntu", ">= 10.04"
