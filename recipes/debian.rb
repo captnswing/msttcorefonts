aee2aae802d35ee7561deba84016c76245084c60
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2012-12-31 17:13:06 +0100

    created different recipes for different platforms

---
+++ recipes/debian.rb
@@ -0,0 +1,10 @@
+package "fontconfig"
+
+# from http://askubuntu.com/questions/16225/how-can-i-accept-microsoft-eula-agreement-for-ttf-mscorefonts-installer
+bash "run ttf-mscorefonts-installer" do
+  user "root"
+  code <<-EOS
+  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+  apt-get install -y ttf-mscorefonts-installer
+  EOS
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,28 +3,18 @@
 # Recipe:: default
 #
 
-# rhel 6 http://oimon.wordpress.com/2011/09/05/msttcorefonts-on-rhel6-centos-6-sl6/
-
-include_recipe "yum::epel"
-
-["cabextract", "ttmkfdir", "xorg-x11-font-utils"].each do |pkg|
-  package pkg
-end
-
-remote_file "#{Chef::Config["file_cache_path"]}/msttcorefonts.spec" do
-  source "http://corefonts.sourceforge.net/msttcorefonts-#{node["msttcorefonts"]["version"]}.spec"
-  action :create_if_missing
-  mode "0644"
-end
-
-execute "rpmbuild msttcorefonts.spec" do
-  cwd Chef::Config["file_cache_path"]
-  # increase the timeout in the spec file before running rpmbuild
-  command "sed -i s/timeout=5/timeout=15/g msttcorefonts.spec; rpmbuild -bb msttcorefonts.spec"
+#---------------
+# install msttcorefonts
+#---------------
+case node["platform"]
+  when "centos", "redhat", "scientific", "fedora"
+    include_recipe "chef-msttcorefonts::redhat"
+  when "ubuntu"
+    include_recipe "chef-msttcorefonts::debian"
 end
 
 #---------------
-# install local.conf
+# install improved local.conf
 #---------------
 # from https://wiki.ubuntu.com/Fonts
 cookbook_file "/etc/fonts/local.conf" do
@@ -33,7 +23,7 @@ cookbook_file "/etc/fonts/local.conf" do
 end
 
 #---------------
-# regenerate font chache
+# regenerate font cache
 #---------------
 bash "regenerate fontchache" do
   code "fc-cache -fv"
