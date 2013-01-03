d9bae0f879ba6dc1f9009d42560ea4b362ffa6c8
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-01-04 00:01:02 +0100

    making recipe idempotent

--- recipes/debian.rb
+++ recipes/debian.rb
@@ -1,18 +1,19 @@
+package "fontconfig"
+
 #--------
 # enable multiverse
 #--------
 # from http://askubuntu.com/a/173562
 execute "enable multiverse" do
-  command "sed -i '/^# deb.*multiverse/ s|^# ||' /etc/apt/sources.list"
+  command "sed -i '/^# deb.*multiverse/ s/^# //' /etc/apt/sources.list"
+  not_if "grep 'deb.*multiverse' /etc/apt/sources.list"
+  # notify "apt-get update" resource found in apt cookbook
+  notifies :run, resources(:execute => "apt-get update"), :immediately
 end
 
 #--------
-# runs apt-get update
+# install mscorefonts
 #--------
-include_recipe "apt"
-
-package "fontconfig"
-
 # from http://askubuntu.com/questions/16225/how-can-i-accept-microsoft-eula-agreement-for-ttf-mscorefonts-installer
 bash "run ttf-mscorefonts-installer" do
   user "root"
@@ -20,4 +21,5 @@ bash "run ttf-mscorefonts-installer" do
   echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
   apt-get install -y ttf-mscorefonts-installer
   EOS
+  creates "/usr/share/fonts/truetype/msttcorefonts/arial.ttf"
 end
